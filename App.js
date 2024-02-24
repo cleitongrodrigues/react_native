5deeeacb228cef35d6aa7013522fa6f4695b2042
Cleiton014 <159494633+Cleiton014@users.noreply.github.com>
2024-02-23 22:49:13 -0300

    Aula props

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Props from './src/Ex_02';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Props mensagem={"Teste"}/>
       <StatusBar style="auto" />
     </View>
   );
