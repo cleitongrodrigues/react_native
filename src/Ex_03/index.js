24f55e229b6a4f4b717c28c89bfb298970196c19
Cleiton014 <159494633+Cleiton014@users.noreply.github.com>
2024-03-08 20:41:55 -0300

    aula 08-03-2024

--- src/Ex_03/index.js
+++ src/Ex_03/index.js
@@ -1,10 +1,40 @@
+import React,{ useState } from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 
 export default function Exemplo_3(){
+    const [numero, setNumero] = useState(0);
+
+    function ola(nome){
+        alert(`Olá ${nome}`);
+
+    function incremento(){
+        const inc = numero + 1;
+        setNumero(inc);
+            // console.log(numero);
+    }
+    }
     return(
         <View style={styles.container}>
-            <Text>Exemplo_3</Text>
+            <Text style={styles.titulo}>Exemplo_3</Text>
+
+            <TouchableOpacity 
+            style={styles.botao} 
+            onPress={() => ola('Cleitin')}>
+
+                <Text style={styles.txtbotao}>button</Text>
+
+            </TouchableOpacity>
+
+            <Text style={styles.titulo}> Valor: {numero} </Text>
+
+            <TouchableOpacity 
+            style={styles.botao} 
+            onPress={() => incremento()}>
+
+                <Text style={styles.txtbotao}>+1</Text>
+
+            </TouchableOpacity>
         </View>
     );
 };
