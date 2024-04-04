a834113298dd19d11b35a26727e33a887afebc3a
CLEITON GUILHERME RODRIGUES DE SOUZA <cleiton.souza50>
2024-04-04 19:18:17 -0300

    mudei a cor

--- src/Ex_05/styles.js
+++ src/Ex_05/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
-        backgroundColor: '#C51162', 
+        backgroundColor: '#FF80AB', 
         justifyContent: 'center',
         padding:8,
         borderRadius: 20, 
